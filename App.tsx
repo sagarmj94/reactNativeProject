4d5dec37f93b424057e8fc707327b7eaf14141e1
apple <apple@apples-MacBook-Pro.local>
2023-10-05 19:07:16 +0530

    input onchange

--- App.tsx
+++ App.tsx
@@ -1,22 +1,39 @@
-import React, { Component } from 'react';
-import { View, TextInput } from 'react-native';
+import React, {Component} from 'react';
+import {View, TextInput, SafeAreaView, StyleSheet} from 'react-native';
 
-interface Props{
-  
-}
-interface State{
-
+interface Props {}
+interface State {
+  textInputValue: string;
 }
 
 class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      textInputValue: '',
+    };
+  }
+
+  updateTextInputValue = (text: any) => {
+    this.setState({textInputValue: text});
+  };
 
   render() {
     return (
-      <View>
-        <TextInput placeholder="Example Input" value={'Sagar'}/>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <TextInput
+          onChangeText={this.updateTextInputValue}
+          placeholder="Example Input"
+          value={this.state.textInputValue}
+          style={{width: 300, borderWidth: 1, alignSelf: 'center'}}
+        />
+      </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {flex: 1, justifyContent: 'center', alignItems: 'stretch'},
+});
 
 export default App;
