16ea18ee2eea6887db4d21ad42c9e5179f94ba48
apple <apple@apples-MacBook-Pro.local>
2023-10-05 21:22:33 +0530

    created simple login form

--- App.tsx
+++ App.tsx
@@ -1,39 +1,68 @@
 import React, {Component} from 'react';
-import {View, TextInput, SafeAreaView, StyleSheet} from 'react-native';
+import {View, TextInput, SafeAreaView, StyleSheet,Button} from 'react-native';
+import Container from './src/component/Container';
 
 interface Props {}
+enum InputType{
+  EMAIL='Email',
+  PASSWORD="Password",
+  LOGIN_BUTTON="Login"
+}
 interface State {
-  textInputValue: string;
+  emailTextValue: string;
+  passwordTextValue:string;
+  
 }
 
 class App extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      textInputValue: '',
+      emailTextValue: '',
+      passwordTextValue:''
     };
   }
 
-  updateTextInputValue = (text: any) => {
-    this.setState({textInputValue: text});
+  updateTextInput = (val:any,type:any) => {
+    switch (type) {
+      case InputType.EMAIL:{
+        this.setState({ emailTextValue: val });
+        break;
+      }
+      case InputType.PASSWORD:{
+        this.setState({passwordTextValue:val})
+        break;
+      }
+    }
   };
+  
+  handleLogin=()=>{
+    console.log("pringting",this.state.emailTextValue,this.state.passwordTextValue);
+  }
 
   render() {
     return (
-      <SafeAreaView style={styles.container}>
+      <Container containerStyles={{alignItems:'center'}}>
         <TextInput
-          onChangeText={this.updateTextInputValue}
-          placeholder="Example Input"
-          value={this.state.textInputValue}
-          style={{width: 300, borderWidth: 1, alignSelf: 'center'}}
+       
+          onChangeText={(val)=>this.updateTextInput(val,InputType.EMAIL)}
+          placeholder={InputType.EMAIL}
+          value={this.state.emailTextValue}
+          style={{width: 300, borderWidth: 1}}
         />
-      </SafeAreaView>
+        <TextInput
+          onChangeText={(val)=>this.updateTextInput(val,InputType.PASSWORD)}
+          placeholder={InputType.PASSWORD}
+          value={this.state.passwordTextValue}
+          style={{width: 300, borderWidth: 1,marginTop:5,marginBottom:5}}
+          secureTextEntry={true}
+        />
+        <Button title={InputType.LOGIN_BUTTON} onPress={this.handleLogin}/>
+      </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {flex: 1, justifyContent: 'center', alignItems: 'stretch'},
-});
+
 
 export default App;
