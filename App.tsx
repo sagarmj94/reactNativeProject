df74fedde089187b0e57e723982d71d9bdfd68bb
apple <apple@apples-MacBook-Pro.local>
2023-10-06 20:08:20 +0530

    focus showing on input next & data submiting after clear inputs

--- App.tsx
+++ App.tsx
@@ -1,65 +1,91 @@
 import React, {Component} from 'react';
-import {View,Text, TextInput, SafeAreaView, StyleSheet,Button} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  SafeAreaView,
+  StyleSheet,
+  Button,
+} from 'react-native';
 import Container from './src/component/Container';
 import CustomButton from './src/component/CustomButton';
 
 interface Props {}
-enum InputType{
-  EMAIL='Email',
-  PASSWORD="Password",
-  LOGIN_BUTTON="Login"
+enum InputType {
+  EMAIL = 'Email',
+  PASSWORD = 'Password',
+  LOGIN_BUTTON = 'Login',
 }
 interface State {
   emailTextValue: string;
-  passwordTextValue:string;
-  
+  passwordTextValue: string;
 }
 
 class App extends Component<Props, State> {
+  private inputRef1: React.RefObject<TextInput> = React.createRef<TextInput>();
   constructor(props: Props) {
     super(props);
     this.state = {
       emailTextValue: '',
-      passwordTextValue:''
+      passwordTextValue: '',
     };
   }
 
-  updateTextInput = (val:any,type:any) => {
+  updateTextInput = (val: any, type: any) => {
     switch (type) {
-      case InputType.EMAIL:{
-        this.setState({ emailTextValue: val });
+      case InputType.EMAIL: {
+        this.setState({emailTextValue: val});
         break;
       }
-      case InputType.PASSWORD:{
-        this.setState({passwordTextValue:val})
+      case InputType.PASSWORD: {
+        this.setState({passwordTextValue: val});
         break;
       }
     }
   };
-  
-  handleLogin=()=>{
-    console.log("pringting",this.state.emailTextValue,this.state.passwordTextValue);
-  }
 
+  handleLogin = () => {
+    console.log(
+      'pringting',
+      this.state.emailTextValue,
+      this.state.passwordTextValue,
+    );
+    
+    this.setState({emailTextValue: ''})
+  
+
+    this.setState({passwordTextValue: ""});
+  };
+  focusOnNextInput = () => {
+    if (this.inputRef1.current) {
+      this.inputRef1.current.focus();
+    }
+  };
   render() {
     return (
-      <Container containerStyles={{alignItems:'center'}}>
-        <Text style={{fontSize:36,marginBottom:10}}>{"Login"}</Text>
+      <Container containerStyles={{alignItems: 'center'}}>
+        <Text style={{fontSize: 36, marginBottom: 10}}>{'Login'}</Text>
         <TextInput
-       
-          onChangeText={(val)=>this.updateTextInput(val,InputType.EMAIL)}
+          onSubmitEditing={this.focusOnNextInput}
+          returnKeyType="next"
+          onChangeText={val => this.updateTextInput(val, InputType.EMAIL)}
           placeholder={InputType.EMAIL}
           value={this.state.emailTextValue}
           style={{width: 300, borderWidth: 1}}
         />
         <TextInput
-          onChangeText={(val)=>this.updateTextInput(val,InputType.PASSWORD)}
+          ref={this.inputRef1}
+          returnKeyType="done"
+          onChangeText={val => this.updateTextInput(val, InputType.PASSWORD)}
           placeholder={InputType.PASSWORD}
           value={this.state.passwordTextValue}
-          style={{width: 300, borderWidth: 1,marginTop:5,marginBottom:5}}
+          style={{width: 300, borderWidth: 1, marginTop: 5, marginBottom: 5}}
           secureTextEntry={true}
         />
-        <CustomButton onPress={this.handleLogin} title={InputType.LOGIN_BUTTON}/>
+        <CustomButton
+          onPress={this.handleLogin}
+          title={InputType.LOGIN_BUTTON}
+        />
       </Container>
     );
   }
